bd0f1ca213c7083671ddc5a42c7c3d5e6b02c44e
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2016-03-06 23:33:43 +0000

    implemented barebones methods in ContactImpl and importing Interface

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -1,8 +1,25 @@
 /**
  * Created by graemewilkinson on 06/03/16.
  */
-public class ContactImpl {
-public int getID() {
-        return 10;
+public class ContactImpl implements Contact {
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getNotes() {
+        return null;
+    }
+
+    @Override
+    public void addNotes(String note) {
+
     }
 }
