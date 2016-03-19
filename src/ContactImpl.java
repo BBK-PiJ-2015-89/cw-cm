c5f1d67bed7ceb678f8b5f578f8bf5480f3c1b46
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2016-03-19 18:05:02 +0000

    added constructor to contact file.

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -2,6 +2,15 @@
  * Created by graemewilkinson on 06/03/16.
  */
 public class ContactImpl implements Contact {
+    int id;
+    String name;
+    String notes;
+
+    public ContactImpl(int id, String name, String notes){
+        id = this.id;
+        name = this.name;
+        notes = this.notes;
+    }
 
     @Override
     public int getId() {
---
+++ src/ContactImplTest.java
@@ -0,0 +1,35 @@
+/**
+ * Created by graemewilkinson on 14/03/16.
+ */
+public class ContactImplTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetNotes() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAddNotes() throws Exception {
+
+    }
+}
