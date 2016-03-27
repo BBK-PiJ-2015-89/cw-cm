79ad5aee786c26827cd05c75cb125cacbe06c895
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2016-03-27 22:21:22 +0100

    pastmeetingtest

--- src/Code/ContactManagerImpl.java
+++ src/Code/ContactManagerImpl.java
@@ -20,21 +20,8 @@ public class ContactManagerImpl implements ContactManager {
         if (contacts == null || date == null){
             throw new NullPointerException();
         }
-
-        if (!(contactList.containsAll(contacts))){
-            throw new IllegalArgumentException();
-        }
         int importContactSize = contacts.size();
-        System.out.println("Contact Size before: " + contacts.size());
-        for (Contact contact: contacts){
-            System.out.println(contact.getName());
-        }
         contacts.retainAll(contactList);
-        System.out.println("Contact Size after: " + contacts.size());
-        for (Contact contact: contacts){
-            System.out.println(contact.getName());
-        }
-
         if (contacts.size() < importContactSize || !(Calendar.getInstance().compareTo(date) < 0)){
             throw new IllegalArgumentException();
         }
@@ -129,21 +116,8 @@ public class ContactManagerImpl implements ContactManager {
 
     @Override
     public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) {
-        /*Optional<Contact> highestContId = contacts.stream().max((a1, a2)-> a1.getId() - a2.getId());
-        if (!(Calendar.getInstance().compareTo(date) > 0) || (highestContId.isPresent() && highestContId.get().getId() > contactID-1)) {
-            throw new IllegalArgumentException();
-        }*/
         int importContactSize = contacts.size();
-        System.out.println("Contact Size before: " + contacts.size());
-        for (Contact contact: contacts){
-            System.out.println(contact.getName());
-        }
         contacts.retainAll(contactList);
-        System.out.println("Contact Size after: " + contacts.size());
-        for (Contact contact: contacts){
-            System.out.println(contact.getName());
-        }
-
         if (contacts.size() < importContactSize || !(Calendar.getInstance().compareTo(date) > 0)){
             throw new IllegalArgumentException();
     }
--- src/Code/ContactManagerImplTest.java
+++ src/Code/ContactManagerImplTest.java
@@ -25,10 +25,11 @@ public class ContactManagerImplTest {
         contactManagerTest.addNewContact("Mark", "Graeme is a  test");
         tempInvalidContactList = new HashSet<>();
         //tempInvalidContactList.add(new ContactImpl(1, "Graeme", "Test Notes"));
-        contactManagerTest.addFutureMeeting(contactManagerTest.getContacts(1), new GregorianCalendar(2016,5,2,12,15));
+        contactManagerTest.addFutureMeeting(contactManagerTest.getContacts(1), new GregorianCalendar(2016,5,2,12,15));//id1
         tempInvalidContactList.add(new ContactImpl(8, "Phil1", "Graeme is a test"));
-        contactManagerTest.addFutureMeeting(contactManagerTest.getContacts(1,2), new GregorianCalendar(2016,5,2,12,10));
-        contactManagerTest.addFutureMeeting(contactManagerTest.getContacts(1), new GregorianCalendar(2016,5,2,12,30));
+        contactManagerTest.addFutureMeeting(contactManagerTest.getContacts(1, 2), new GregorianCalendar(2016, 5, 2, 12, 10));//id2
+        contactManagerTest.addFutureMeeting(contactManagerTest.getContacts(1), new GregorianCalendar(2016,5,2,12,30));//id3
+        contactManagerTest.addNewPastMeeting(contactManagerTest.getContacts(1), new GregorianCalendar(2015,2,12), "This meeting was good");//id4
 
     }
 
@@ -58,7 +59,8 @@ public class ContactManagerImplTest {
 
     @Test
     public void testGetPastMeeting() throws Exception {
-
+        Meeting old = contactManagerTest.getPastMeeting(4);
+        Assert.assertEquals(1, old.getId());
     }
 
     @Test
@@ -123,7 +125,9 @@ public class ContactManagerImplTest {
     public void testAddNewPastMeetingwithValidContact(){
         Set<Contact> tempValidContactList = new HashSet<>();
         tempValidContactList.add(new ContactImpl(2, "Phil1", "Phil is a test"));
-        contactManagerTest.addNewPastMeeting(tempValidContactList, new GregorianCalendar(2014,2,4), "This is another test meeting");
+        contactManagerTest.addNewPastMeeting(tempValidContactList, new GregorianCalendar(2014, 2, 4), "This is another test meeting");
+        List<Meeting> listReturned = contactManagerTest.getMeetingListOn(new GregorianCalendar(2014, 2, 4));
+        Assert.assertEquals(1, listReturned.size());
 
 
     }
